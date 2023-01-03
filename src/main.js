5682212bcbc1ba42618fa58da1e9c362ba44167c
Qusz <de.gorchakov@gmail.com>
2023-01-03 12:28:03 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+// CSS
+import 'normalize.css';
+import '@/styles/main.scss';
+
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  }
+})
+
+
+
+createApp(App)
+.use(router)
+.use(vuetify)
+.mount('#app')
