df24fd9a018e7618fac888fb51e54bccd76631fc
Qusz <de.gorchakov@gmail.com>
2023-01-09 12:16:47 +0300

    removes drawer

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,48 +1,14 @@
 <template>
   <v-app-bar 
     color="secondary-darken-1"
+    class="text-center"
   >
-    <v-app-bar-nav-icon 
-      variant="text"
-      absolute 
-      @click.stop="drawer = !drawer"
-    ></v-app-bar-nav-icon>
-    <v-app-bar-title class="text-md-h5">
+    <v-app-bar-title class="text-md-h4">
       The Flags
     </v-app-bar-title>
   </v-app-bar>
-   <v-navigation-drawer
-      v-model="drawer"
-      location="left"
-      temporary
-      width="220"
-      class="elevation-5"
-    >
-    <v-list nav>
-      <v-list-item 
-        prepend-icon="mdi-web-plus" 
-        title="New Game" 
-        value="inbox"
-      ></v-list-item>
-    </v-list>
-  </v-navigation-drawer>
 </template>
 
-<script>
+<script></script>
 
-import { ref } from 'vue';
-
-export default {
-  setup() {
-    const drawer = ref(false);
-
-    return {
-      drawer
-    }
-  }
-}
-</script>
-
-<style>
-
-</style>
+<style></style>
