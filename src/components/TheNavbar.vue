2466c17b925c50dc7930710c1b00476a123b4086
Qusz <de.gorchakov@gmail.com>
2023-01-09 12:54:52 +0300

    changes font-size of the navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,7 +3,7 @@
     color="secondary-darken-1"
     class="text-center"
   >
-    <v-app-bar-title class="text-md-h4">
+    <v-app-bar-title class="text-md-h5 font-weight-bold">
       The Flags
     </v-app-bar-title>
   </v-app-bar>
